970567d475cd7aad2d4ffcfa168b912ecde01bba
ricardo-ervilha <ricardoervilhacomp@gmail.com>
2025-08-05 21:42:46 -0300

    Inicia S2S para CPP. Preparação para merge com semântico pois preciso do Env

---
+++ antlr/src/MainCPP.java
@@ -0,0 +1,45 @@
+package src;
+
+import ast.Node;
+import parser.*;
+
+import org.antlr.v4.runtime.*;
+import util.SyntaxErrorListener;
+import visitors.CPPVisitor;
+import visitors.InterpretVisitor;
+
+public class MainCPP {
+    
+    public static void main(String[] args){
+        try {
+            // Criar o stream de caracteres do arquivo
+            CharStream stream = CharStreams.fromFileName(args[0]);
+
+            // Inicializar lexer e parser
+            LangLexer lex = new LangLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lex);
+            LangParser parser = new LangParser(tokens);
+
+            // Remover os listeners padrão e adicionar o customizado
+            parser.removeErrorListeners();
+            parser.addErrorListener(SyntaxErrorListener.INSTANCE);
+
+            // Não construa árvore sintática padrão (usando AST customizada)
+            parser.setBuildParseTree(false);
+
+            // Tenta fazer o parse do programa
+            Node ast = parser.prog().ast;
+
+            // SOURCE-TO-SOURCE
+            CPPVisitor iv = new CPPVisitor();
+            // ast.accept(iv);
+
+            // Se chegou até aqui, não houve erro sintático
+            System.out.println("accept");
+
+        } catch (Exception e) {
+            // Em caso de qualquer erro de parsing
+            System.out.println("reject: "+e.getMessage());
+        }
+    }
+}
